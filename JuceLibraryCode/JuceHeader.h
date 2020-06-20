eb0970200c587529739a623038042546b956fd64
gmcgoldr <garrin.mcgoldrick@gmail.com>
2020-06-20 19:23:59 -0400

    update version number

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -51,7 +51,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "ResonantAmp";
     const char* const  companyName    = "Resonant DSP";
-    const char* const  versionString  = "0.4.2";
-    const int          versionNumber  = 0x402;
+    const char* const  versionString  = "0.5.0";
+    const int          versionNumber  = 0x500;
 }
 #endif
--- package/setup.iss
+++ package/setup.iss
@@ -1,6 +1,6 @@
 [Setup]
 AppName=ResonantAmp
-AppVersion=0.4.2
+AppVersion=0.5.0
 DefaultDirName={commoncf64}\VST3
 DirExistsWarning=no
 DisableDirPage=no
