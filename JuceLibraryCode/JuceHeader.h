41d6610452a472d5764cbe307900f0ecfe327f77
gmcgoldr <garrin.mcgoldrick@gmail.com>
2020-06-21 12:33:57 -0400

    increase version number

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -51,7 +51,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "ResonantAmp";
     const char* const  companyName    = "Resonant DSP";
-    const char* const  versionString  = "0.5.3";
-    const int          versionNumber  = 0x503;
+    const char* const  versionString  = "0.6.0";
+    const int          versionNumber  = 0x600;
 }
 #endif
