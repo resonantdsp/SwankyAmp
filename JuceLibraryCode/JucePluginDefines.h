abbbb571c6b175f9482c939c4409016d5ed95de6
gmcgoldr <garrin.mcgoldrick@gmail.com>
2020-09-21 16:53:59 -0400

    increase version number

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -74,13 +74,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  0
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                0.10.0
+ #define JucePlugin_Version                0.10.1
 #endif
 #ifndef  JucePlugin_VersionCode
- #define JucePlugin_VersionCode            0xa00
+ #define JucePlugin_VersionCode            0xa01
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "0.10.0"
+ #define JucePlugin_VersionString          "0.10.1"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
--- package/setup.iss
+++ package/setup.iss
@@ -1,6 +1,6 @@
 [Setup]
 AppName=SwankyAmp
-AppVersion=0.10.0
+AppVersion=0.10.1
 DefaultDirName={commoncf64}\VST3
 DirExistsWarning=no
 DisableDirPage=no
