34942a8862529b7498b260c3d4e6e671f27c3d30
gmcgoldr <garrin.mcgoldrick@gmail.com>
2020-11-02 10:36:11 -0500

    add push pull amp tests

---
+++ Source/Tests/Tests_PushPullAmp.cpp
@@ -0,0 +1,16 @@
+#include "../dsp/PushPullAmp.h"
+
+#include "catch.hpp"
+
+TEST_CASE("scale buffer scales a buffer")
+{
+	std::vector<FAUSTFLOAT*> bufferCh = {nullptr};
+	std::vector<FAUSTFLOAT> bufferMem = {1.0f, 1.1f, 1.2f, 1.3f};
+	bufferCh[0] = &(bufferMem[0]);
+
+	scaleBuffer((int)bufferMem.size(), &bufferCh[0], 2.0f);
+
+	REQUIRE(bufferMem[0] == Approx(2.0f));
+	REQUIRE(bufferMem[3] == Approx(2.6f));
+}
+
