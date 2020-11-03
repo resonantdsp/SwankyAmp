1a1d97d35eccad4cdc4f1474b13b023990039102
gmcgoldr <garrin.mcgoldrick@gmail.com>
2020-11-03 10:55:34 -0500

    refactor parameter handling in presets
    * convert from prior preset versions only during load
    * move parameter mapping utilities to utils

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -23,6 +23,7 @@
 #include <JuceHeader.h>
 
 #include "PluginEditor.h"
+#include "Utils.h"
 
 #include "PluginProcessor.h"
 
@@ -122,99 +123,6 @@ SwankyAmpAudioProcessor::SwankyAmpAudioProcessor() :
 
 SwankyAmpAudioProcessor::~SwankyAmpAudioProcessor() {}
 
-/**
- * @brief Remap a float value to a positive and negative range.
- *
- * Remap the range [-1, 0) to [`to_low`, 0) and the range (0, 1] to
- * (0, `to_high`]. Note that the values below zero can be scaled differently
- * from those above zero.
- *
- * @param unit value to remap, usually in the unit scale [-1, +1]
- * @param to_low scale values above zero by this amount
- * @param to_high  scale values below zero by this amount
- * @return remapped value
- */
-float remap_sided(float unit, float to_low, float to_high)
-{
-  if (unit >= 0) { return unit * to_high; }
-  else
-  {
-    return -unit * to_low;
-  }
-}
-
-/**
- * @brief Remap a float value to a new range.
- *
- * Remap the range [-1, +1] to [`to_low`, `to_high`]. Unlike `remap_sided`, the
- * same scaling is applied to all input values. An input value of `0` will map
- * to the mean of `to_low` and `to_high`.
- *
- * @param unit value to remap, usually in the unit scale [-1, +1]
- * @param to_low value to reach when `unit` is at `-1`
- * @param to_high value to reach when `unit` is at `+1`
- * @return remapped value
- */
-float remap_range(float unit, float to_low, float to_high)
-{
-  return (unit + 1.0f) / 2.0f * (to_high - to_low) + to_low;
-}
-
-/**
- * @brief Remap a given range of floats to a new range.
- *
- * Remap the range [`x1`, `x2`] to the range [`y1`, `y2`]. Clips the input `x`
- * to the range [`x1`, `x2`].
- *
- * @param x value to remap
- * @param x1 lower value in the range to map from
- * @param x2 upper value in the range to map from
- * @param y1 lower value in the range to map to
- * @param y2 upper value in the range to map to
- * @return remapped value
- */
-float remap_xy(float x, float x1, float x2, float y1, float y2)
-{
-  x = jmax(x1, jmin(x2, x));
-  x = (x - x1) / (x2 - x1);
-  float y = x * (y2 - y1) + y1;
-  return y;
-}
-
-/**
- * @brief Remap a value from the range (-1, +1) to the same range, but with a
- * new metric such that dy/dx(x) follows a sinh curve.
- *
- * In other words, for an input x near the eges -1 or 1, a small change in x
- * will result in a bigger change in y. For input values near 0, a small
- * change in x will result in a smaller change in y.
- *
- * The calulating uses exponential values in the intermediate steps. This can
- * probably be fixed later, but for now scales should stay below 100.
- *
- * @param x the value to remap
- * @param x0 the point in the range (-1, 1) where y changes most slowly
- * @param scale larger values will create a bigger difference
- * @return remaped value
- */
-float remapSinh(float x, float x0, float scale)
-{
-  // the value mapped by sinh
-  const float mapped = sinh((x - x0) * scale);
-  // y(x) at the bounds -1 and 1
-  const float lower = sinh((-1.0f - x0) * scale);
-  const float upper = sinh((+1.0f - x0) * scale);
-  return (mapped - lower) / (upper - lower) * 2.0f - 1.0f;
-}
-
-float invertRemapSinh(float x, float x0, float scale)
-{
-  const float lower = sinh((-1.0f - x0) * scale);
-  const float upper = sinh((+1.0f - x0) * scale);
-  const float unscaled = (x + 1.0f) / 2.0f * (upper - lower) + lower;
-  return asinh(unscaled) / scale + x0;
-}
-
 // set the amp object user parameters from the VTS values
 void SwankyAmpAudioProcessor::setAmpParameters()
 {
@@ -228,7 +136,7 @@ void SwankyAmpAudioProcessor::setAmpParameters()
     amp_channel[i].set_input_level(*parInputLevel);
     amp_channel[i].set_output_level(
         *parOutputLevel
-        + (10.0f + remap_xy(preAmpDriveMap, 0.0f, 1.0f, 0.0f, -3.0f)) / 35.0f);
+        + (10.0f + remapXY(preAmpDriveMap, 0.0f, 1.0f, 0.0f, -3.0f)) / 35.0f);
     amp_channel[i].set_triode_drive(preAmpDriveMap);
     amp_channel[i].set_tetrode_drive(powerAmpDriveMap);
 
@@ -243,55 +151,55 @@ void SwankyAmpAudioProcessor::setAmpParameters()
 
     amp_channel[i].set_cabinet_on((*parCabOnOff > 0.5f) ? true : false);
     amp_channel[i].set_cabinet_brightness(
-        remap_sided(*parCabBrightness, -0.6f, +0.6f));
+        remapSided(*parCabBrightness, -0.6f, +0.6f));
     amp_channel[i].set_cabinet_distance(*parCabDistance);
     // full dynamic when knob is at 0.0
     amp_channel[i].set_cabinet_dynamic(
-        remap_xy(*parCabDynamic, -1.0f, 0.0f, -1.0f, +1.0f));
+        remapXY(*parCabDynamic, -1.0f, 0.0f, -1.0f, +1.0f));
     // move the dynamic level down over the dynamic knob range
     amp_channel[i].set_cabinet_dynamic_level(-1.0f * *parCabDynamic);
 
-    amp_channel[i].set_triode_hp_freq(remap_sided(*parLowCut, -1.0f, +0.75f));
-    amp_channel[i].set_tetrode_hp_freq(remap_sided(*parLowCut, -1.0f, +0.75f));
+    amp_channel[i].set_triode_hp_freq(remapSided(*parLowCut, -1.0f, +0.75f));
+    amp_channel[i].set_tetrode_hp_freq(remapSided(*parLowCut, -1.0f, +0.75f));
 
     const float minPreAmpTight =
-        remap_xy(preAmpDriveMap, -0.5f, +1.0f, -1.0f, 0.0f);
+        remapXY(preAmpDriveMap, -0.5f, +1.0f, -1.0f, 0.0f);
     const float adjPreAmpTight =
-        remap_range(*parPreAmpTight, minPreAmpTight, +1.0f);
+        remapRange(*parPreAmpTight, minPreAmpTight, +1.0f);
 
     amp_channel[i].set_triode_grid_tau(
-        remap_sided(adjPreAmpTight * -1.0f, -0.5f, +0.1f));
+        remapSided(adjPreAmpTight * -1.0f, -0.5f, +0.1f));
     amp_channel[i].set_triode_grid_ratio(
-        remap_sided(adjPreAmpTight * -1.0f, -1.0f, +0.1f));
+        remapSided(adjPreAmpTight * -1.0f, -1.0f, +0.1f));
     amp_channel[i].set_triode_plate_bias(
-        remap_sided(adjPreAmpTight, -1.0f, +0.5f));
+        remapSided(adjPreAmpTight, -1.0f, +0.5f));
     amp_channel[i].set_triode_plate_comp_ratio(
-        remap_sided(adjPreAmpTight, -1.0f, +0.0f));
+        remapSided(adjPreAmpTight, -1.0f, +0.0f));
 
     amp_channel[i].set_triode_grid_level(
-        remap_sided(*parPreAmpGrit * -1.0f, -0.2f, +3.0f));
+        remapSided(*parPreAmpGrit * -1.0f, -0.2f, +3.0f));
     amp_channel[i].set_triode_grid_clip(
-        remap_sided(*parPreAmpGrit * -1.0f, -1.0f, +4.0f));
+        remapSided(*parPreAmpGrit * -1.0f, -1.0f, +4.0f));
     amp_channel[i].set_triode_plate_comp_level(
-        remap_sided(*parPreAmpGrit * +1.0f, -0.0f, +1.0f));
+        remapSided(*parPreAmpGrit * +1.0f, -0.0f, +1.0f));
     amp_channel[i].set_triode_plate_comp_offset(
-        remap_sided(*parPreAmpGrit * -1.0f, -0.0f, +5.0f));
+        remapSided(*parPreAmpGrit * -1.0f, -0.0f, +5.0f));
 
     amp_channel[i].set_tetrode_grid_tau(
-        remap_sided(*parPowerAmpTight * -1.0f, -1.0f, +1.0f));
+        remapSided(*parPowerAmpTight * -1.0f, -1.0f, +1.0f));
     amp_channel[i].set_tetrode_grid_ratio(
-        remap_sided(*parPowerAmpTight * -1.0f, -1.0f, +0.1f));
+        remapSided(*parPowerAmpTight * -1.0f, -1.0f, +0.1f));
     amp_channel[i].set_tetrode_plate_comp_depth(
-        remap_sided(*parPowerAmpTight * -1.0f, -0.5f, +0.0f));
+        remapSided(*parPowerAmpTight * -1.0f, -0.5f, +0.0f));
     amp_channel[i].set_tetrode_plate_sag_tau(
-        remap_sided(*parPowerAmpTight * -1.0f, -1.0f, +1.0f));
+        remapSided(*parPowerAmpTight * -1.0f, -1.0f, +1.0f));
 
     amp_channel[i].set_tetrode_plate_sag_depth(
         powerAmpSagMap +
         // shift the depth higher at low drive to get some audible effect when
         // not much of signal is over clip, and lower at high drive to avoid
         // just hacking away the signal with a constant db offset
-        remap_xy(powerAmpDriveMap, -1.0f, 1.0f, 1.0f, -1.0f));
+        remapXY(powerAmpDriveMap, -1.0f, 1.0f, 1.0f, -1.0f));
     amp_channel[i].set_tetrode_plate_sag_ratio(*parPowerAmpSagRatio);
     amp_channel[i].set_tetrode_plate_sag_onset(powerAmpSagMap);
     amp_channel[i].set_tetrode_plate_sag_factor(
@@ -531,125 +439,6 @@ void SwankyAmpAudioProcessor::setStateInformation(
   }
 }
 
-/**
- * @brief Build map of serialized state parameter names to values.
- * @param state serialized state
- * @return map of parameter names to values
- */
-std::unordered_map<String, double>
-mapParameterValues(const SerializedState& state)
-{
-  std::unordered_map<String, double> values;
-
-  XmlElement* element = state->getFirstChildElement();
-
-  while (element != nullptr)
-  {
-    if (element->getTagName() == "PARAM" && element->hasAttribute("id")
-        && element->hasAttribute("value"))
-    {
-      const String& id = element->getStringAttribute("id");
-      const double value = element->getDoubleAttribute("value");
-      values[id] = value;
-    }
-
-    element = element->getNextElement();
-  }
-
-  return values;
-}
-
-double transformUnitScale(
-    double value,
-    double lower,
-    double upper,
-    double lowerPost,
-    double upperPost)
-{
-  const double post = 2.0 / (upperPost - lowerPost)
-          * ((value + 1.0) / 2.0 * (upper - lower) + lower - lowerPost)
-      - 1.0;
-  return jmin(1.0, jmax(-1.0, post));
-}
-
-struct VersionNumber
-{
-  int major = -1;
-  int minor = -1;
-  int patch = -1;
-
-  VersionNumber() {}
-
-  VersionNumber(int major, int minor, int patch) :
-      major(major), minor(minor), patch(patch)
-  {
-  }
-
-  bool operator==(const VersionNumber& other) const
-  {
-    return this->major == other.major && this->minor == other.minor
-        && this->patch == other.patch;
-  }
-
-  bool operator<(const VersionNumber& other) const
-  {
-    if (this->major < other.major) return true;
-    if (this->major == other.major && this->minor < other.minor) return true;
-    if (this->major == other.major && this->minor == other.minor
-        && this->patch < other.patch)
-      return true;
-    return false;
-  }
-
-  bool operator>(const VersionNumber& other) const
-  {
-    return !(*this == other) && !(*this < other);
-  }
-
-  bool operator!=(const VersionNumber& other) const
-  {
-    return !(*this == other);
-  }
-};
-
-VersionNumber parseVersionString(const String& versionString)
-{
-  int section = 0;
-  VersionNumber versionNumber;
-  String buff;
-  for (const auto c : versionString)
-  {
-    if (c == '.')
-    {
-      if (buff.isNotEmpty())
-      {
-        if (section == 0)
-          versionNumber.major = buff.getIntValue();
-        else if (section == 1)
-          versionNumber.minor = buff.getIntValue();
-        else if (section == 2)
-          versionNumber.patch = buff.getIntValue();
-      }
-      section += 1;
-      buff.clear();
-      continue;
-    }
-    buff += c;
-  }
-
-  if (buff.isNotEmpty())
-  {
-    if (section == 0)
-      versionNumber.major = buff.getIntValue();
-    else if (section == 1)
-      versionNumber.minor = buff.getIntValue();
-    else if (section == 2)
-      versionNumber.patch = buff.getIntValue();
-  }
-
-  return versionNumber;
-}
-
 void SwankyAmpAudioProcessor::setStateInformation(
     const std::unique_ptr<XmlElement>& state,
     const String& presetText,
@@ -659,67 +448,6 @@ void SwankyAmpAudioProcessor::setStateInformation(
 
   std::unordered_map<String, double> values;
   if (state != nullptr) values = mapParameterValues(state);
-
-  // TODO: move to a function
-
-  // range changes from 0.6 to 0.7
-  if (state != nullptr && state->hasAttribute("pluginVersion")
-      && parseVersionString(state->getStringAttribute("pluginVersion"))
-          < VersionNumber(0, 7, 0))
-  {
-    if (values.find("idPowerAmpDrive") != values.end())
-    {
-      const double value = values["idPowerAmpDrive"];
-      const double post =
-          transformUnitScale(value, log(1.0), log(1e3), log(0.5), log(5e2));
-      values["idPowerAmpDrive"] = post;
-    }
-    if (values.find("idPowerAmpSag") != values.end())
-    {
-      const double value = values["idPowerAmpSag"];
-      const double post = transformUnitScale(value, 0.0, 1.0, 0.0, 0.5);
-      values["idPowerAmpSag"] = post;
-    }
-  }
-
-  // from 1.1 to 1.2
-  if (state != nullptr && state->hasAttribute("pluginVersion")
-      && parseVersionString(state->getStringAttribute("pluginVersion"))
-          < VersionNumber(1, 2, 0))
-  {
-    if (values.find("idPreAmpDrive") != values.end())
-    {
-      const double value = values["idPreAmpDrive"];
-      const float post = remap_xy((float)value, -1.0f, 1.0f, -0.67f, 0.86f);
-      values["idPreAmpDrive"] = (double)post;
-    }
-    if (values.find("idPowerAmpDrive") != values.end())
-    {
-      const double value = values["idPowerAmpDrive"];
-      const float post = invertRemapSinh(
-          remap_xy((float)value, -1.0f, 1.0f, -0.85f, 1.14f), -0.2f, 1.0f);
-      values["idPowerAmpDrive"] = (double)post;
-    }
-    if (values.find("idPowerAmpSag") != values.end())
-    {
-      const double value = values["idPowerAmpSag"];
-      const float post = remapSinh((float)value, 0.0f, 1.0f);
-      values["idPowerAmpSag"] = (double)post;
-    }
-  }
-
-  // from 1.3.0 to 1.3.1
-  if (state != nullptr && state->hasAttribute("pluginVersion")
-      && parseVersionString(state->getStringAttribute("pluginVersion"))
-          < VersionNumber(1, 3, 1))
-  {
-    if (values.find("idPreAmpDrive") != values.end())
-    {
-      const double value = values["idPreAmpDrive"];
-      const float post = invertRemapSinh((float)value, 0.5f, 1.0f);
-      values["idPreAmpDrive"] = (double)post;
-    }
-  }
 
   for (const auto& id : parameterIds)
   {
