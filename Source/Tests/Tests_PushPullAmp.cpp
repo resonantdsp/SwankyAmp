d3618ce7d7e0ba90aeaf3915437197cf91ea0605
gmcgoldr <garrin.mcgoldrick@gmail.com>
2020-11-03 08:27:59 -0500

    format code

--- Source/Tests/Tests_PushPullAmp.cpp
+++ Source/Tests/Tests_PushPullAmp.cpp
@@ -4,13 +4,12 @@
 
 TEST_CASE("scale buffer scales a buffer")
 {
-	std::vector<FAUSTFLOAT*> bufferCh = {nullptr};
-	std::vector<FAUSTFLOAT> bufferMem = {1.0f, 1.1f, 1.2f, 1.3f};
-	bufferCh[0] = &(bufferMem[0]);
+  std::vector<FAUSTFLOAT*> bufferCh = {nullptr};
+  std::vector<FAUSTFLOAT> bufferMem = {1.0f, 1.1f, 1.2f, 1.3f};
+  bufferCh[0] = &(bufferMem[0]);
 
-	scaleBuffer((int)bufferMem.size(), &bufferCh[0], 2.0f);
+  scaleBuffer((int)bufferMem.size(), &bufferCh[0], 2.0f);
 
-	REQUIRE(bufferMem[0] == Approx(2.0f));
-	REQUIRE(bufferMem[3] == Approx(2.6f));
+  REQUIRE(bufferMem[0] == Approx(2.0f));
+  REQUIRE(bufferMem[3] == Approx(2.6f));
 }
-
