ee977476982e8df87dbbbe233e30fd292ea218f6
zacanger <zac@zacanger.com>
2022-07-23 17:08:15 -0600

    fix typo, add safety to build script

--- package/build-mac.sh
+++ package/build-mac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 cp ../Resources/dmg-bg.png dmg-vst3/.bg.png
 cp ../Resources/dmg-bg.png dmg-au/.bg.png
@@ -9,7 +10,7 @@ cp -r ../Builds/MacOSX/build/Release/SwankyAmp.component dmg-au/
 hdiutil create -fs "Case-sensitive Journaled HFS+" -format UDRW -srcfolder dmg-vst3 dmg-vst3.dmg
 hdiutil create -fs "Case-sensitive Journaled HFS+" -format UDRW -srcfolder dmg-au dmg-au.dmg
 
-read -n 1 -p "Set DMG style then press any key to continue"
+read -r -n 1 -p "Set DMG style then press any key to continue"
 
 hdiutil convert -format UDRO -o SwankyAmp-vst3.dmg dmg-vst3.dmg
 hdiutil convert -format UDRO -o SwankyAmp-au.dmg dmg-au.dmg
